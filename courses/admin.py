a7727f7aeaad1bc40ab06f0daf402bd29cd46ac9
Huutoan1709 <nguyenhuutoan010@gmail.com.com>
2024-03-27 22:55:05 +0700

    first push

---
+++ courses/admin.py
@@ -0,0 +1,88 @@
+from django.contrib import admin
+from django.template.response import TemplateResponse
+
+from .models import Category, Course, Lesson, Tag
+from django.utils.html import mark_safe
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django.urls import path
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ['pk', 'name']
+    search_fields = ['name']
+    list_filter = ['id', 'name']
+
+
+class LessonTagInline(admin.TabularInline):
+    model = Lesson.tag.through
+
+
+class LessonInlineAdmin(admin.StackedInline):
+    model = Lesson
+    pk_name = 'course'
+
+
+
+
+class CourseAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all':('/static/CSS/main.css',)
+        }
+
+    inlines = (LessonInlineAdmin,)
+    list_display = ['pk', 'subject', 'description']
+    search_fields = ['subject']
+    readonly_fields = ['img']
+
+
+    def img(self, course):
+        if course:
+            return mark_safe(
+                "<img src='/static/{url}' width='120'/>".format(url=course.image.name, alt=course.subject)
+            )
+
+
+class LessonForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display =['id', 'subject', 'create_date', 'active']
+    form = LessonForm
+
+    inlines = (LessonTagInline,)
+
+
+class CourseAppAdminSite(admin.AdminSite):
+    site_header = 'HE THONG QUAN LY KHOA HOC'
+
+    def get_urls(self):
+        return [
+            path('/course-stats/', self.course_stats)
+        ] + super().get_urls()
+
+
+    def course_stats(self, request):
+
+        course_count = Course.objects.count()
+
+        return TemplateResponse(request, 'admin/course-stats.html', {
+            'course_count': course_count
+        })
+
+
+admin_site = CourseAppAdminSite('mycourse')
+
+
+admin_site.register(Category, CategoryAdmin)
+admin_site.register(Course, CourseAdmin)
+admin_site.register(Lesson, LessonAdmin)
+admin_site.register(Tag)
+
+
---
+++ courses/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from ckeditor.fields import RichTextField
+
+
+class User(AbstractUser):
+    pass
+
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+    create_date = models.DateField(auto_now_add=True, null= True)
+    update_date = models.DateField(auto_now=True,null= True)
+    active = models.BooleanField(default=True)
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50, null= False)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
+    subject = models.CharField(max_length=250, null =False)
+
+    def __str__(self):
+        return self.subject
+class Course(BaseModel,ItemBase):
+
+    class Meta:
+        unique_together = ('subject','category')
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='courses/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    tag = models.ManyToManyField('Tag')
+
+class Lesson(BaseModel,ItemBase):
+    class Meta:
+        unique_together = ('subject','course')
+    content = RichTextField()
+    image = models.ImageField(upload_to='lessons/%Y/%m')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    tag = models.ManyToManyField('Tag')
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
